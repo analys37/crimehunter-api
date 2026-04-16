2be5f4e9805eae9aca4ca90f53f6a89b4b65d87f
analys37 <analyssumbar37@gmail.com>
2026-04-16 08:06:57 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,3 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("crimehunter.my.id 🚀");
-});
-
-module.exports = app;
+export default function handler(req, res) {
+  res.status(200).send("crimehunter server on🚀");
+}
